1509bacc59a5eeb9efe831db33b83f2c44d2ca30
Laken Hafner <laken.hafner@outlook.com>
2017-02-27 18:39:53 -0500

    create league creation page

---
+++ league.php
@@ -0,0 +1,24 @@
+<?php
+include_once "includes/classes.php";
+require_once "../vendor/autoload.php";
+
+$loader = new Twig_Loader_Filesystem("templates");
+$twig = new Twig_Environment($loader);
+
+if (!isset($id)) {
+	echo "<script>window.location.replace('login.php');</script>";
+	exit("redirecting...");
+}
+
+$getData = new getData();
+$league = $getData->getLeague();
+if ($league == null) {
+	echo $twig->render("createleague.twig", array("name" => $name));
+	
+	if (isset($_POST["createLeague"])) {
+		echo "test";
+	} 
+} else {
+	echo "you have a league";
+}
+?>
